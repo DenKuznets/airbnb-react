e46505387544fda1abd4fea94f46efa2782cfcc6
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-01-28 15:15:27 +0300

    .nav--logo added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import logoUrl from '../assets/airbnb1-logo.png'
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <img src={logoUrl} alt='airbnb logo' />
+      <img className='nav--logo' src={logoUrl} alt='airbnb logo' />
     </nav>
   )
 }
